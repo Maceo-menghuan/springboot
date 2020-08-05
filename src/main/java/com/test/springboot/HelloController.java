40e1a1476b2758713234d29dffe92e3acd8fd69b
e <wh_yinhanwei@yunlizhihui.com>
2020-08-05 20:55:11 +0800

    springboot hello项目第一次提交

---
+++ src/main/java/com/test/springboot/HelloController.java
@@ -0,0 +1,19 @@
+package com.test.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试控制器
+ *
+ * @author: @殷函为
+ * @create: 2020-8-03
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello1")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
